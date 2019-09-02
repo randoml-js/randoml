eeb72bdc98a4a6ab85ea8a0468ac21d38fa54ca4
Jakub Biesiada <biesiadajakub@icloud.com>
2019-09-02 22:43:53 +0200

    Update

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+import RandoML from './randoml';
+export default RandoML;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import RandoML from './randoml';
+
+export default RandoML;
+exports['default'] = RandoML;
+module.exports = exports['default'];
